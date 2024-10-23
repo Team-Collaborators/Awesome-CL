738ff915e480910534f53c65e932bb2c97bc799f
andobrucki <annadobrucki@gmail.com>
2024-10-23 13:25:15 +0200

    add playground and not foundin routes

---
+++ src/components/Playground/Playground.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Playground.scss";
+
+const Playground = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Playground</h1>
+      <p>This is the space to test new components</p>
+    </div>
+  );
+};
+
+export default Playground;
