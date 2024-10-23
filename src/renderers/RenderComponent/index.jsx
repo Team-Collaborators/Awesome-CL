5569e197a3b57dbc939e59b61d38863933996ed8
IrYna-Kononova <iryna.kononova@dci-studen.org>
2024-10-23 14:19:36 +0200

    added an installation part in render component

--- src/renderers/RenderComponent/index.jsx
+++ src/renderers/RenderComponent/index.jsx
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { RenderCode } from "../RenderCode"; 
+import { RenderCode } from "../RenderCode";
+import styles from "./RenderComponent.module.css";
 
-//This component is rendering the controllers but could also be used on
-//other pages. !! The Json have to be the same as the controller.json 
+// This component renders the controllers but could also be used on
+// other pages. The JSON has to be the same as the controller.json
 
-export function RenderComponent({data}) {
-    
+export function RenderComponent({ data }) {
   const { name } = useParams();
   const [component, setComponent] = useState(null);
+  const [selectedInstall, setSelectedInstall] = useState(null);
 
   useEffect(() => {
     if (data && data.length >= 2) {
       const foundComponent = data.find((comp) => comp.title === name);
       setComponent(foundComponent);
+      setSelectedInstall(foundComponent.installation[0]); //default to first method CLI
     } else {
-    setComponent(data[0])
+      setComponent(data[0]);
+      setSelectedInstall(data[0]?.installation[0]); //default to first method CLI
     }
   }, [name, data]);
-  if(component){
+
+  // const handleInstallClick = (install) => {
+  //  setSelectedInstall(install); //update the selected installation method
+  //};
+
+  if (component) {
     return (
-    <section>
+      <section>
         <h2>Installation</h2>
-        <ul>
-        {component.installation.map((elem) => {
-            return(
-                <li key={elem.name}>
-                    <button>
-                        <img src={elem.path} alt={elem.name} />
-                        <span>{elem.name}</span>
-                        <RenderCode code={elem.code} />
-                    </button>
-                </li>
-
-            )
-        })}
+        <ul className={styles.installList}>
+          {component.installation.map((elem) => {
+            return (
+              <li key={elem.name}>
+                <button
+                  className={`${styles.installButton} ${
+                    selectedInstall?.name === elem.name ? styles.active : ""
+                  }`} //add active class if selected
+                  onClick={() => setSelectedInstall(elem)}
+                >
+                  <img
+                    src={elem.path}
+                    alt={elem.name}
+                    className={styles.icon}
+                  />
+                  <span>{elem.name}</span>
+                </button>
+              </li>
+            );
+          })}
         </ul>
-    </section>        
 
-    //   <div>
-    //     <h2>{component.title}</h2>
-    //     <p>{component.description}</p>
-    //     <RenderCode data={component.controllers} />
-    //   </div>
-    )
+        {selectedInstall && (
+          <div style={{ marginTop: "20px" }}>
+            <RenderCode code={selectedInstall.command} />
+          </div>
+        )}
+      </section>
+    );
   }
+
+  return null;
 }
