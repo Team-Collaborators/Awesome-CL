2ec035eaf9cd791b3359a26f6ae265e3c61dbb73
Heriberto Santana <heriberto.santana@dci-student.org>
2024-10-23 11:11:06 +0200

    NewRender

--- src/renderers/RenderComponent/index.jsx
+++ src/renderers/RenderComponent/index.jsx
@@ -1,48 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { RenderCode } from "../RenderCode"; 
+import { RenderCode } from "../RenderCode";
 
-//This component is rendering the controllers but could also be used on
-//other pages. !! The Json have to be the same as the controller.json 
 
-export function RenderComponent({data}) {
-    
+
+export function RenderComponent({ data }) {
   const { name } = useParams();
   const [component, setComponent] = useState(null);
+  const [installation, setInstallation] = useState("cli-command-to-install");
 
   useEffect(() => {
     if (data && data.length >= 2) {
       const foundComponent = data.find((comp) => comp.title === name);
       setComponent(foundComponent);
     } else {
-    setComponent(data[0])
+      setComponent(data[0]);
     }
   }, [name, data]);
-  if(component){
+  if (component) {
     return (
-    <section>
+      <section>
         <h2>Installation</h2>
         <ul>
-        {component.installation.map((elem) => {
-            return(
-                <li key={elem.name}>
-                    <button>
-                        <img src={elem.path} alt={elem.name} />
-                        <span>{elem.name}</span>
-                        <RenderCode code={elem.code} />
-                    </button>
-                </li>
-
-            )
-        })}
+          {component.installation.map((elem) => {
+            return (
+              <li key={elem.name}>
+                <button onClick={()=> setInstallation(elem.command)}>
+                  <img src={elem.path} alt={elem.name} />
+                  <span>{elem.name}</span>
+                </button>
+              </li>
+            );
+          })}
         </ul>
-    </section>        
+        <RenderCode code={installation} />
+      </section>
 
-    //   <div>
-    //     <h2>{component.title}</h2>
-    //     <p>{component.description}</p>
-    //     <RenderCode data={component.controllers} />
-    //   </div>
-    )
+    
+    );
   }
 }
