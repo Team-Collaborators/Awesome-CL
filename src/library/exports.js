95f50df4330cd720a060cca9e65773d142373159
kwossi <krissi.stoecker@gmail.com>
2024-10-24 16:15:46 +0200

    maybe now

--- src/library/exports.js
+++ src/library/exports.js
@@ -1,8 +1,9 @@
 //! TO DO:
 
+import Button from "../component_testing/Button/Button";
 // import all components from the json file, then export them all
 
 // example but can be done with a loop after importing the json from the public folder
 // import Button from "./components/Button";
 
-// export { Button };
+export { Button };
