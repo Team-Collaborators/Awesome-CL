b7c28871f827fbb32e10d6c5c9c1c8a1217cbb98
NancyJK <jahaucii@gmail.com>
2024-10-28 10:01:03 +0100

    adding footer

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,68 +5,72 @@ import { HomePage } from "../pages/HomePage/HomePage";
 import Navbar from "../components/Navbar/Navbar";
 import Playground from "../components/Playground/Playground";
 import NotFound from "../components/NotFound/NotFound";
+import Footer from "../components/Footer/Footer";
 
 import { RenderPages } from "../components/renderers/RenderPages/RenderPages";
 
 export default function MainRoutes({ isDarkMode, toggleTheme }) {
   return (
     <Router>
-      <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-      <div className="content-wrapper">
-        <Routes>
-          {/* Home page route */}
-          <Route index path="/" element={<HomePage />} />
-          {/* Frontend routes */}
-          <Route path="frontend">
-            <Route
-              path="components/*"
-              element={<RenderPages file={"frontend/components"} />}
-            />
-            <Route
-              path="hooks/*"
-              element={<RenderPages file={"frontend/hooks"} />}
-            />
-            <Route
-              path="routes/*"
-              element={<RenderPages file={"frontend/routes"} />}
-            />
-          </Route>
-          {/* Backend routes */}
-          <Route path="backend">
-            <Route
-              path="configurations/*"
-              element={<RenderPages file={"backend/configurations"} />}
-            />
-            <Route
-              path="controllers/*"
-              element={<RenderPages file={"backend/controllers"} />}
-            />
-            <Route
-              path="middlewares/*"
-              element={<RenderPages file={"backend/middlewares"} />}
-            />
-            <Route
-              path="models/*"
-              element={<RenderPages file={"backend/models"} />}
-            />
-            <Route
-              path="tests/*"
-              element={<RenderPages file={"backend/tests"} />}
-            />
-            <Route
-              path="utils/*"
-              element={<RenderPages file={"backend/utils"} />}
-            />
-            <Route
-              path="envFiles/*"
-              element={<RenderPages file={"backend/envFiles"} />}
-            />
-          </Route>
-          {/* Playground route */}
-          <Route path="playground" element={<Playground />} />
-          {/* Catch-all route for 404 */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+      <div className="app-container">
+        <Navbar isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+        <div className="content-wrapper">
+          <Routes>
+            {/* Home page route */}
+            <Route index path="/" element={<HomePage />} />
+            {/* Frontend routes */}
+            <Route path="frontend">
+              <Route
+                path="components/*"
+                element={<RenderPages file={"frontend/components"} />}
+              />
+              <Route
+                path="hooks/*"
+                element={<RenderPages file={"frontend/hooks"} />}
+              />
+              <Route
+                path="routes/*"
+                element={<RenderPages file={"frontend/routes"} />}
+              />
+            </Route>
+            {/* Backend routes */}
+            <Route path="backend">
+              <Route
+                path="configurations/*"
+                element={<RenderPages file={"backend/configurations"} />}
+              />
+              <Route
+                path="controllers/*"
+                element={<RenderPages file={"backend/controllers"} />}
+              />
+              <Route
+                path="middlewares/*"
+                element={<RenderPages file={"backend/middlewares"} />}
+              />
+              <Route
+                path="models/*"
+                element={<RenderPages file={"backend/models"} />}
+              />
+              <Route
+                path="tests/*"
+                element={<RenderPages file={"backend/tests"} />}
+              />
+              <Route
+                path="utils/*"
+                element={<RenderPages file={"backend/utils"} />}
+              />
+              <Route
+                path="envFiles/*"
+                element={<RenderPages file={"backend/envFiles"} />}
+              />
+            </Route>
+            {/* Playground route */}
+            <Route path="playground" element={<Playground />} />
+            {/* Catch-all route for 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </Router>
   );
