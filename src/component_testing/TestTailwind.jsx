201bfcac37df63a6b0c02d1112459cfb3ce988e4
andobrucki <annadobrucki@gmail.com>
2024-10-28 15:22:23 +0100

    edit playground button and work on different styling

--- src/component_testing/TestTailwind.jsx
+++ src/component_testing/TestTailwind.jsx
@@ -1,6 +1,9 @@
 const TestTailwind = () => {
-    return <div className="bg-pink-500 p-4 mt-5 mb-2 text-white shadow md:shadow-lg hover:bg-pink-300 cursor-pointer">Tailwind Test Component - only Tailwind styles</div>;
-  };
-  
-  export default TestTailwind;
-  
+  return (
+    <div className="bg-pink-500 p-4 mt-5 mb-2 w-2/3 mx-auto text-white shadow md:shadow-lg hover:bg-pink-300 cursor-pointer">
+      Tailwind Test Component - only Tailwind styles
+    </div>
+  );
+};
+
+export default TestTailwind;
