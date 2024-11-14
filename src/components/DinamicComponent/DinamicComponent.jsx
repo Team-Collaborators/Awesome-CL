587584c6934868ebe68df2bdf9e5ca34be548211
kwossi <krissi.stoecker@gmail.com>
2024-11-14 16:27:41 +0100

    accordion json started, dynamicComponent updated to render nested components, what a piece of work lol.

--- src/components/DinamicComponent/DinamicComponent.jsx
+++ src/components/DinamicComponent/DinamicComponent.jsx
@@ -2,25 +2,37 @@ import React from "react";
 
 import { Button, Input } from "AwesomeCL"; // Importa tus componentes
 import Modal from "../../component_testing/Modal/Modal";
+import Accordion from "../../component_testing/Accordion/Accordion";
+import AccordionItem from "../../component_testing/Accordion/AccordionItem";
+import Search from "../../component_testing/Search/Search";
 
 const components = {
   //Object with all components
   Button,
   Input,
   Modal,
+  Accordion,
+  AccordionItem,
+  Search,
 };
 
 export default function DynamicComponent({ type, props, children }) {
-  const ComponentToRender = components[type]; // Find the component on the object
-
-  try {
-    if (!ComponentToRender) {
-      throw new Error(`Component "${type}" not found.`);
-    }
-
-    return React.createElement(ComponentToRender, props, children); //Create the component
-  } catch (error) {
-    console.error("Error rendering example:", error);
-    return <p>Error rendering component</p>;
+  const ComponentToRender = components[type];
+  if (!ComponentToRender) {
+    console.error(`Error: Component "${type}" not found.`);
+    return <p>Error: Component "{type}" not found.</p>;
   }
+
+  const childrenElements = Array.isArray(children)
+    ? children.map((child) => {
+        console.log(typeof child.type);
+        return DynamicComponent({
+          type: child.type,
+          props: child.props,
+          children: child.children,
+        });
+      })
+    : children;
+
+  return React.createElement(ComponentToRender, props, childrenElements);
 }
