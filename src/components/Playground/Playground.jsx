db0b0bb6c432f1222bf60bdf1c8a5baf64cbcc1c
andobrucki <annadobrucki@gmail.com>
2024-11-21 14:38:47 +0100

    playground

--- src/components/Playground/Playground.jsx
+++ src/components/Playground/Playground.jsx
@@ -14,8 +14,7 @@ const Playground = () => {
     <div className="playground-wrapper">
       <h1>Playground</h1>
       <p>This is the space to test new components</p>
-      <br />
-      <h2>Card Component</h2>
+
       <div
         className="container"
         style={{
@@ -24,65 +23,7 @@ const Playground = () => {
           gap: "20px",
           padding: "20px",
         }}
-      >
-        <Card
-          image={<Image src="https://via.placeholder.com/300" alt="Example" />}
-          header={<h3>Card default Title</h3>}
-          footer={<button>Action</button>}
-          style={{ maxWidth: "300px" }}
-          actions={
-            <>
-              <Button
-                color="green"
-                radius="full"
-                style={{ backgroundColor: "yellow", color: "tomato" }}
-              >
-                Action 1
-              </Button>
-              <Button color="secondary">Action 2</Button>
-            </>
-          }
-        >
-          <p>
-            This is the body content of the card. It is flexible and reusable!
-          </p>
-        </Card>
-        <br />
-        <Card
-          variant="outlined"
-          size="md"
-          header={<h3>Card outlined Title</h3>}
-          footer={<button>Action</button>}
-          actions={
-            <>
-              <Button variant="primary">Action 1</Button>
-              <Button variant="secondary">Action 2</Button>
-            </>
-          }
-        >
-          {" "}
-          <p>
-            This is the body content of the card. It is flexible and reusable!
-          </p>
-        </Card>
-        <Card
-          variant="elevated"
-          size="md"
-          header={<h3>Card Elevated Title</h3>}
-          footer={<button>Action</button>}
-          actions={
-            <>
-              <Button variant="primary">Action 1</Button>
-              <Button variant="secondary">Action 2</Button>
-            </>
-          }
-        >
-          {" "}
-          <p>
-            This is the body content of the card. It is flexible and reusable!
-          </p>
-        </Card>
-      </div>
+      ></div>
     </div>
   );
 };
